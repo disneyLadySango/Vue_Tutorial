06f56508f04c07f6f439c39545c89949d122f223
disneyLadySango <sangota.d.duck@gmail.com>
2018-12-24 17:48:27 +0900

    はじめに完了
    チュートリアルはじめに完了

--- sample/js/sample.js
+++ sample/js/sample.js
@@ -18,3 +18,59 @@ var app3 = new Vue({
     seen : true
   }
 })
+
+var app4 = new Vue({
+  el : '#app-4',
+  data : {
+    todos : [
+      { text : 'Learn JavaScript' },
+      { text : 'Learn Vue.js' },
+      { text : 'Build something awesome' }
+    ]
+  }
+})
+
+var app5Message = '';
+
+var app5 = new Vue({
+  el : '#app-5',
+  data :{
+    message : 'Tutorial Vue.js!'
+  },
+  methods :{
+    reverseMessage : function(){
+      this.message = this.message.split('').reverse().join('')
+    },
+    changeHW : function(){
+      if(this.message === 'Hello World!' ||
+      this.message === 'Hello World!'.split('').reverse().join('')){
+        this.message = app5Message
+      } else {
+        app5Message = this.message
+        this.message = 'Hello World!'
+      }
+    }
+  }
+})
+
+var app6 = new Vue({
+  el : '#app-6',
+  data : {
+    message : 'Hello Vue.js!'
+  }
+})
+
+Vue.component('todo-item', {
+  props : ['todo'],
+  template : '<li>{{ todo.text }}</li>'
+})
+var app7 = new Vue({
+  el : '#app-7',
+  data : {
+    groceryList : [
+      { id : 0 , text : 'Vegetables' },
+      { id : 1 , text : 'Cheese' },
+      { id : 2 , text : 'Whatever else humans are supposed to eat' },
+    ]
+  }
+})
